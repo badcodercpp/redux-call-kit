1b0a3ed345a67217a7f01005d4b27f10c70f1330
ajay <ajaykrjha93@gmail.com>
2019-02-20 05:06:38 +0530

    middleware corrected

--- src/redux/middleWare/index.js
+++ src/redux/middleWare/index.js
@@ -4,7 +4,7 @@ export const  rtcPeerConnection = (connection) => {
       return next => (action) => {
         if (typeof action === 'function') {
           return action(dispatch, getState);
-        }else if( typeof action === 'object' && action.rtcPeerConnection !== undefined ){
+        }else if( typeof action === 'object' && action.rtcPeerConnection !== undefined && typeof action.rtcPeerConnection ==='function' ){
             return action.rtcPeerConnection(connection, dispatch, getState)
         }
         return next(action);
@@ -17,7 +17,7 @@ export const  socketConnection = (connection) => {
     return next => (action) => {
       if (typeof action === 'function') {
         return action(dispatch, getState);
-      }else if( typeof action === 'object' && action.socket !== undefined ){
+      }else if( typeof action === 'object' && action.socket !== undefined && typeof action.socket === 'function' ){
           return action.socket(connection, dispatch, getState)
       }
       return next(action);
@@ -30,7 +30,7 @@ export const  rtcNavigator = (navigator) => {
     return next => (action) => {
       if (typeof action === 'function') {
         return action(dispatch, getState);
-      }else if( typeof action === 'object' && action.rtcNavigator !== undefined ){
+      }else if( typeof action === 'object' && action.rtcNavigator !== undefined && typeof action.rtcNavigator === 'function' ){
           return action.rtcNavigator(navigator, dispatch, getState)
       }
       return next(action);
